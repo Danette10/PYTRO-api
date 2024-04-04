efce193585fbd44373e62c7e86e6b2a2b7d8a28c
Dan Sebag <dsebag@asptt.com>
2024-04-04 21:09:41 +0200

    update

--- app.py
+++ app.py
@@ -155,4 +155,4 @@ def handle_disconnect():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
